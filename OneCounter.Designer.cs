d35f83f37b7d9a2695e1974761952bfd0b8ef884
Martin Pellicer <martinpelli@outlook.com>
2022-02-18 00:03:46 -0300

    FrontEnd created

---
+++ OneCounter.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace OneCounter
+{
+    partial class OneCounter
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterButton = new System.Windows.Forms.Button();
+            this.tittleLabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(104, 188);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(125, 39);
+            this.enterButton.TabIndex = 0;
+            this.enterButton.Text = "Calculate";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.OnEnterButtonClicked);
+            // 
+            // tittleLabel
+            // 
+            this.tittleLabel.AutoSize = true;
+            this.tittleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittleLabel.Location = new System.Drawing.Point(76, 45);
+            this.tittleLabel.Name = "tittleLabel";
+            this.tittleLabel.Size = new System.Drawing.Size(182, 31);
+            this.tittleLabel.TabIndex = 1;
+            this.tittleLabel.Text = "Enter Number";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(91, 107);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(153, 30);
+            this.inputTextBox.TabIndex = 2;
+            this.inputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // OneCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 281);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.tittleLabel);
+            this.Controls.Add(this.enterButton);
+            this.Name = "OneCounter";
+            this.Text = "One Counter";
+            this.Load += new System.EventHandler(this.OneCounter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label tittleLabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+    }
+}
+
---
+++ OneCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneCounter
+{
+    public partial class OneCounter : Form
+    {
+        public OneCounter()
+        {
+            InitializeComponent();
+        }
+
+        private void OneCounter_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void OnEnterButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
